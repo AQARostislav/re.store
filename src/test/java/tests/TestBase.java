798905068a3db5bc46a18f6052f792b75676ad34
Rostislav <aqarostislav@gmail.com>
2026-02-03 16:17:29 +0300

    added screenshot

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -18,8 +18,6 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 10_000;
         Configuration.pageLoadStrategy = "eager";
-
-        Configuration.browser = "chrome";
     }
 
     @BeforeEach
